967b63df34ed229f80eecf0f0d3576f61797ac11
Mouad Oumous <mouadoumous@gmail.com>
2025-05-20 20:40:08 +0200

    enhance app

--- app/src/main/java/io/droidevs/taskjournal/domain/usecase/category/UpdateCategoryUseCase.kt
+++ app/src/main/java/io/droidevs/taskjournal/domain/usecase/category/UpdateCategoryUseCase.kt
@@ -1,13 +1,19 @@
 package io.droidevs.taskjournal.domain.usecase.category
 
+import io.droidevs.taskjournal.domain.dispatchers.CoroutineDispatcherProvider
 import io.droidevs.taskjournal.domain.model.Category
 import io.droidevs.taskjournal.domain.repository.CategoryRepository
 import io.droidevs.taskjournal.domain.result.Result
 import io.droidevs.taskjournal.domain.result.errors.DataError
+import io.droidevs.taskjournal.domain.result.errors.DatabaseError
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class UpdateCategoryUseCase @Inject constructor(
-    private val repository: CategoryRepository
+    private val repository: CategoryRepository,
+    private val dispatchers: CoroutineDispatcherProvider
 ) {
-    suspend operator fun invoke(category: Category): Result<Unit, DataError.DatabaseError> = repository.updateCategory(category)
+    suspend operator fun invoke(category: Category): Result<Unit, DatabaseError> = withContext(dispatchers.io){
+        repository.updateCategory(category)
+    }
 } 
